c5351d1b1444e9d6775a46fd1ab70e99e90465fa
gelavllnv <maryangelavillanueva0795@gmail.com>
2026-08-28 14:51:11 +0800

    feat: update web.php routes for student registration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('students.create');
+});
+
+Route::controller(StudentController::class)->group(function () {
+    Route::get('/students', 'index')->name('students.index');
+    Route::get('/students/create', 'create')->name('students.create');
+    Route::post('/students', 'store')->name('students.store');
+    Route::get('/students/{student}', 'show')->name('students.show');
 });
